0153c175e090546496a2c0a5f23699c9236f42c8
ozwalf <yuyugunfire6@gmail.com>
2024-03-18 22:42:07 -0700

    Revert "00-0- maybe"
    This reverts commit 90299f8a02a0f70420666d55e6aded0c539cb09f.

--- 3DFPS_Platformer/Assets/Scripts/PlayerController.cs
+++ 3DFPS_Platformer/Assets/Scripts/PlayerController.cs
@@ -1,131 +1,76 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Net.Http.Headers;
-using System.Reflection;
-using System.Security.Cryptography;
 using UnityEngine;
-using UnityEngine.UIElements;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float gravity;
+    public float jumpForce;
+    private bool isGrounded;
+    private CharacterController charController;
+    [SerializeField] Transform cameraTransform;
+    private Vector3 velocity; // Added velocity vector for smoother jumping
 
-    private CharacterController character;
-
-    private float current_position;
-    private float desire_position;
-    private float previous_position;
-    private float displacement;
-
-    private bool upwards;
-    private bool downwards;
-    private bool on_ground;
-
-    public float velocity;
-    public float jump_height;
-    public float acceleration;
-    public float delta_time;
-
-    private float left_right;
-    private float forward_backward;
-    public float sensitivity;
-
-
-    void Awake() {
-        character = GetComponent<CharacterController>();
-        current_position = transform.position.y;
-        desire_position = 0f;
-        previous_position = transform.position.y;
-        displacement = 0f;
-
-        upwards = false;
-        downwards = true;
-        on_ground = false;
+    void Start()
+    {
+        charController = GetComponent<CharacterController>();
     }
 
-    void Start() {
-        velocity = 7.5f;
-        jump_height = 3f;
-        delta_time = 1f;
-        acceleration = 2 * (jump_height - velocity * delta_time) / (delta_time * delta_time);
-        left_right = 0f;
-        forward_backward = 0f;
-        sensitivity = 9.8f;
-    }
-
-    void Update() {
-
-        vertical_collision();
-
-        vertical_motion();
-        
-          // sprint
-        float temp = sensitivity;
-        if (Input.GetKey(KeyCode.LeftShift)) {
-            temp = sensitivity * 2;
-        }        
-
-        
-        movement(temp);
-
-    }
-
-
-    private void vertical_collision() {
-         if(Math.Abs(previous_position - current_position) <= 0) {
-            if (!upwards) {
-                on_ground = true;
-                downwards = false;
-                upwards = false;
-            } else {
-                downwards = true;
-                upwards = false;
-            }
+    void Update()
+    {
+        // Check for jump input
+        if (isGrounded && Input.GetButtonDown("Jump"))
+        {
+            Jump();
         }
     }
 
-    private void vertical_motion() {
-        if (Input.GetAxisRaw("Jump") != 0 && on_ground) {
-            desire_position = transform.position.y + jump_height;
-            upwards = true;
-            on_ground = false;
-        } 
-
-        if (upwards) {
-            if (velocity >= 3f) {
-                velocity += acceleration * Time.deltaTime;
-            }
-            displacement = velocity * Time.deltaTime;
-        }
+    void FixedUpdate()
+    {
+        GroundCheck();
         
-        if (downwards) {
-            if (velocity <= 7.5f) {
-                velocity -= acceleration * Time.deltaTime;
-            }
-            displacement = -velocity * Time.deltaTime;}
+        // Movement
+        float deltaX = Input.GetAxis("Horizontal") * speed * Time.fixedDeltaTime;
+        float deltaZ = Input.GetAxis("Vertical") * speed * Time.fixedDeltaTime;
 
-        if(current_position >= desire_position) {
-            upwards = false;
-            downwards = true;
-            velocity = 9f;
+        // Transform input according to camera rotation
+        Vector3 moveDirection = cameraTransform.TransformDirection(new Vector3(deltaX, 0f, deltaZ));
+
+
+        // Apply gravity
+        if (!isGrounded)
+        {
+            // Apply gravity gradually
+            velocity.y += -gravity * Time.fixedDeltaTime;
         }
+
+        // Move the player
+        charController.Move(moveDirection + velocity * Time.fixedDeltaTime);
     }
 
-    private void movement(float temp_sensitivity) {
-        left_right = Input.GetAxis("Horizontal") * Time.deltaTime * temp_sensitivity;
-        forward_backward = Input.GetAxis("Vertical") * Time.deltaTime * temp_sensitivity;
-
-        Vector3 up_down = transform.up * displacement;
-        Vector3 lef_righ = transform.right * left_right;
-        Vector3 for_back = transform.forward * forward_backward;
-
-        Vector3 move = up_down + lef_righ + for_back;
-
-        previous_position = transform.position.y;
-
-        character.Move(move);
-
-        current_position = transform.position.y;
+    void Jump()
+    {
+        // Set initial jump velocity
+        velocity.y = jumpForce;
+        isGrounded = false;
     }
 
+    void GroundCheck()
+    {
+        // Perform a Raycast from the player's feet
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, 1.3f))
+        {
+            // Check if the Raycast hits an object tagged as "Ground"
+            if (hit.collider.CompareTag("Ground"))
+            {
+                isGrounded = true;
+                return;
+            } else if(hit.collider.CompareTag("MovingPlatform")) {
+                isGrounded = true;
+                return;
+            }
+        }
 
+        isGrounded = false;
+    }
 }
