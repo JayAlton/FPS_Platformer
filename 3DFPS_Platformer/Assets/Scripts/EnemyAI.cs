7ba4c30e818de9d55ca4b4ab0c309bf237bf7a68
ozwalf <yuyugunfire6@gmail.com>
2024-03-20 15:16:02 -0700

    j

--- 3DFPS_Platformer/Assets/Scripts/EnemyAI.cs
+++ 3DFPS_Platformer/Assets/Scripts/EnemyAI.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Numerics;
-using Unity.PlasticSCM.Editor.WebApi;
 using UnityEngine;
 
 public class EnemyAI : MonoBehaviour
--- 3DFPS_Platformer/Assets/Scripts/MainMenu.cs
+++ 3DFPS_Platformer/Assets/Scripts/MainMenu.cs
@@ -12,4 +12,9 @@ public class MainMenu : MonoBehaviour
     public void PlayGame () {
         SceneManager.LoadSceneAsync(1);
     }
+
+    public void QuitGame ()
+    {
+        Application.Quit();
+    }
 }
